595672fe44ae44077a573c4e8ce561a6145567b9
yash <yashvarpe2005@gmail.com>
2023-09-19 23:53:53 +0530

    fetch URL from api check method

--- weatherio-starter-master/assets/js/api.js
+++ weatherio-starter-master/assets/js/api.js
@@ -1,6 +1,45 @@
 /**
  * @license MIT
  * @fileoverview All api related stuff like api_key, api request etc.
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
+ * @copyright yash_varpe 2023 All rights reserved
+ * @author yash_varpe <yashvarpe2005@gmail.com>
  */
+
+"use strict ";
+
+const api_key = "7fb0f71797c059a1ba3cb7a387646478";
+
+/**
+ * Fetch data from server
+ * @param {String} URL API url
+ * @param {Function} callback callback
+ */
+
+export const fetchData = (URL, callback) => {
+	fetch(`${URL}%app_id=${api_key}`)
+		.then((res) => res.json())
+		.then((data) => callback(data));
+};
+
+export const url = {
+	currentWeather(lat, lon) {
+		return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`;
+	},
+	forecast(lat, lon) {
+		return `https://api.openweathermap.org/data/2.5/forecast?${lan}&${lon}&units=metric`;
+	},
+	airPollution(lat, lon) {
+		return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
+	},
+	reversGeo(lat, lon) {
+		return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
+	},
+	/**
+	 *
+	 * @param {string} query Searching query eg. 'India' , Maharashtra,London ....etc
+	 * @returns
+	 */
+	geo(query) {
+		return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
+	},
+};
