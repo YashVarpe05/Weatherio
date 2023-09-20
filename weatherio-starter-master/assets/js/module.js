eaa8b779d8a331a610ef0c50e64b8fc5c9994d37
yash <yashvarpe2005@gmail.com>
2023-09-20 22:16:53 +0530

    added the month section

--- weatherio-starter-master/assets/js/module.js
+++ weatherio-starter-master/assets/js/module.js
@@ -4,3 +4,36 @@
  * @copyright codewithsadee 2023 All rights reserved
  * @author codewithsadee <mohammadsadee24@gmail.com>
  */
+// todo: add the 7 day and months and make the function that is use to generate or get the date and time
+
+export const weekDayNames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday",
+];
+
+export const monthName = [
+	"Jan",
+	"Feb",
+	"Mar",
+	"Apr",
+	"May",
+	"Jun",
+	"Jul",
+	"Aug",
+	"Sep",
+	"Oct",
+	"Nov",
+	"Dec",
+];
+
+export const getData = (dateUnix, timezone) => {
+	const date = new Date((dateUnix + timezone) * 1000);
+	const weekDayNames = weekDayNames[date.getUTCDay()];
+	const monthName = monthName[date.getUTCMonth()];
+	return `${weekDayNames} ${date.getUTCDate()},${monthName} `;
+};
