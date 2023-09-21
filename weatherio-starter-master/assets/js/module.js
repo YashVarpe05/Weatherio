7e71c3bddfc405094bbad3565837e2220de06b52
yash <yashvarpe2005@gmail.com>
2023-09-21 19:09:51 +0530

    route the current location

--- weatherio-starter-master/assets/js/module.js
+++ weatherio-starter-master/assets/js/module.js
@@ -31,9 +31,68 @@ export const monthName = [
 	"Dec",
 ];
 
+/**
+ *
+ * @param {number} dateUnix Unix date in seconds
+ * @param {number} timezone TimeZone shift from UTC to seconds
+ * @returns {string} Date String. Format:"Sunday 10,Jan"
+ */
+
 export const getData = (dateUnix, timezone) => {
 	const date = new Date((dateUnix + timezone) * 1000);
 	const weekDayNames = weekDayNames[date.getUTCDay()];
 	const monthName = monthName[date.getUTCMonth()];
 	return `${weekDayNames} ${date.getUTCDate()},${monthName} `;
+};
+/**
+ *
+ * @param {number} timeUnix Unix date in seconds
+ * @param {number} timezone TimeZone shift from UTC in seconds
+ * @returns {string} timeSharing format :'HH AM/PM'
+ */
+
+export const getTime = (timeUnix, timezone) => {
+	const date = new Date((timeUnix + timezone) * 1000);
+	const hours = date.getUTCHours();
+	const period = hours >= 12 ? "PM" : "AM";
+
+	return `${hour % 12 || 12} ${period}`;
+};
+
+/**
+ * @param {number} mps Meter per seconds
+ * @returns {number} kilometer per hours
+ */
+
+export const mps_to_kmh = (mps) => {
+	const mph = mps * 3600;
+	return mph / 1000;
+};
+
+export const aqiText = {
+	1: {
+		level: "Good",
+		message:
+			"Air quality is considered satisfactory, and air pollution poses little or no risk",
+	},
+	2: {
+		level: "Fair",
+		message:
+			"Air quality is acceptable; however , for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.",
+	},
+	3: {
+		level: "Moderate",
+		message:
+			"Members of sensitive groups may experience health effects. The general public is not likely to be affected.",
+	},
+	4: {
+		level: "Poor",
+		message:
+			"Everyone may begin experience heath effects ; members of sensitive group may experience more serious health effects.",
+	},
+	5: {
+		level: "Very Poor",
+		message:
+			"Air quality is acceptable; However , for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.",
+	},
 };
--- weatherio-starter-master/assets/js/route.js
+++ weatherio-starter-master/assets/js/route.js
@@ -4,3 +4,50 @@
  * @copyright codewithsadee 2023 All rights reserved
  * @author codewithsadee <mohammadsadee24@gmail.com>
  */
+
+"use strict";
+
+import { query } from "express";
+import { updateWeather, error404 } from "./app.js";
+const defaultLocation = "#weather?lat=19.997454&lon=73.789803"; //Nashik location
+
+const currentLocation = () => {
+	window.navigator.geolocation.getCurrentPosition(
+		(res) => {
+			const { latitude, longitude } = res.coords;
+			updateWeather(`lat=${latitude}&lon=${longitude}`);
+		},
+		(err) => {
+			window.location.hash = defaultLocation;
+		}
+	);
+};
+
+/**
+ * @param {string} query Searched query
+ */
+
+const searchedLocation = (query) => updateWeather(...query.split("&"));
+//
+const routes = new Map([
+	["/current-location", currentLocation],
+	["/weather", searchedLocation],
+]);
+
+const checkHash = () => {
+	const requestURL = window.location.hash.slice(1);
+
+	const [route, query] = requestURL.includes
+		? requestURL.split("?")
+		: [requestURL];
+
+	routes.get(routes) ? routes.get(routes)(query) : error404();
+};
+window.addEventListener("hashchange", checkHash);
+window.addEventListener("load", () => {
+	if (!window.location.hash) {
+		window.location.hash = "#/current-location";
+	} else {
+		checkHash();
+	}
+});
